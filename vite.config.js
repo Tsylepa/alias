7af3cbb6528064043466f54ab827cc052ff5be77
tsylepa <tsylepa@gmail.com>
2023-08-14 11:46:39 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/alias/",
+  base: "/",
   plugins: [react()],
   resolve: {
     alias: {
