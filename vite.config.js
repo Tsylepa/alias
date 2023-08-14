c78d40b5c9e39b573249f743e74ca41b6fb37281
tsylepa <tsylepa@gmail.com>
2023-08-14 11:23:53 +0100

    Add base

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
+  base: "/alias/",
   plugins: [react()],
   resolve: {
     alias: {
-      src: "/src", // Аліас для вашого src директорії
+      src: "/src",
     },
   },
 });
