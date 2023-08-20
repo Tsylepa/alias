5882d46b903251a4a908392284d4033d9a442955
tsylepa <tsylepa@gmail.com>
2023-08-20 22:20:16 +0100

    Added teams and results

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,31 @@ import "./App.css";
 import _ from "lodash";
 import words from "./words.json";
 import Game from "./components/Game";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { switchScreen } from "./redux/game/gameSlice";
 import { updateWordsCollection } from "./redux/game/gameSlice";
+import { getScreen } from "./redux/game/gameSelectors";
+import Results from "./components/Results";
+import Round from "./components/Round";
+import Teams from "./components/Teams";
 
 const App = () => {
   const dispatch = useDispatch();
   const shuffledWords = _.shuffle(words);
+  const screen = useSelector(getScreen);
 
   useEffect(() => {
     dispatch(updateWordsCollection(shuffledWords));
-    dispatch(switchScreen("round"));
   }, [dispatch, shuffledWords]);
-
-  return <Game />;
+  return (
+    <>
+      <button onClick={() => dispatch(switchScreen("teams"))}>Screen</button>
+      {screen === "game" && <Game />}
+      {screen === "results" && <Results />}
+      {screen === "round" && <Round />}
+      {screen === "teams" && <Teams />}
+    </>
+  );
 };
 
 export default App;
