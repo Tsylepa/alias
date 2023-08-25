7a613d398b00b24d7523d28170e741cf3e596753
tsylepa <tsylepa@gmail.com>
2023-08-25 12:27:26 +0100

    Add getWinScore

--- src/redux/game/gameSelectors.js
+++ src/redux/game/gameSelectors.js
@@ -2,6 +2,8 @@ export const getCurrentWord = (state) => state.game.currentWord;
 
 export const getTime = (state) => state.game.time;
 
+export const getWinScore = (state) => state.game.winScore;
+
 export const getTimeIsUp = (state) => state.game.timeIsUp;
 
 export const getIsPlaying = (state) => state.game.playing;
